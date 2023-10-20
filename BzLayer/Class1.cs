fb9e6e4a48add34c25778914b19a8d57a871d450
Adityakumardeshmukh <deshmukhaditya810@gmail.com>
2023-10-20 11:37:04 +0000

    uploded files

---
+++ BzLayer/Class1.cs
@@ -0,0 +1,15 @@
+﻿
+using Entity;
+using DataLayer;
+namespace BzLayer;
+
+public class MovieBz
+{
+    public List<Movie> GetMovies()
+    {
+    DataAccess dataAccess = new DataAccess();
+    return dataAccess.GetMovies();
+    }
+
+}
+
---
+++ Entity/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Entity;
+public class Movie
+{
+    public int id {get; set;}
+    public string Name {get;set;}
+
+    public int Ratings {get; set;}
+
+    public int Ryear {get; set;}
+}
